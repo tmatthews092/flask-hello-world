058a48759e4210aab12f255ffd8f03f4c4295418
Theo Matthews <tmatthews092@gmail.com>
2026-07-22 22:28:20 -0600

    commented out safe pattern

--- app.py
+++ app.py
@@ -9,3 +9,23 @@ conn = psycopg2.connect(DATABASE_URL)
 @app.route("/")
 def index():
     return "Hello World from YOUR NAME in 3308"
+
+# conn = None
+# cur = None
+# try:
+#     conn = psycopg2.connect(DATABASE_URL)
+#     cur = conn.cursor()
+
+#     # SQL work goes here
+
+#     conn.commit()
+#     return "Success message here"
+# except Exception as e:
+#     if conn is not None:
+#         conn.rollback()
+#     return f"Database error: {e}"
+# finally:
+#     if cur is not None:
+#         cur.close()
+#     if conn is not None:
+#         conn.close()
