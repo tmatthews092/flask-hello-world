ae11689e45c80ab30d96fc26e9f1f24756bc6643
Theo Matthews <tmatthews092@gmail.com>
2026-07-22 21:58:47 -0600

    push part 3 custom route and message

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route("/")
+def index():
+    return "Hello World from YOUR NAME in 3308"
