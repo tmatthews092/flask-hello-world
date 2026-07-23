cb86d5bd62ed37397b67e22672b9e245acd19b0c
Theo Matthews <tmatthews092@gmail.com>
2026-07-22 22:18:41 -0600

    database psycopg2 + os

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route("/")
 def index():
